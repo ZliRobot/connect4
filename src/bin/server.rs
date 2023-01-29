6bdef462d37d9065a9c8165b92c850c974c89aef
ZliRobot <dusanradovan@gmail.com>
2023-01-29 19:50:43 +0100

    Randomly choose the first player

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,5 +1,6 @@
 use connect4::{connections, Player, PlayersConnection};
-use std::error::Error;
+use rand;
+use std::{error::Error, mem};
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -7,14 +8,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
     loop {
-        let player1 = PlayersConnection {
+        let mut player1 = PlayersConnection {
             socket: listener.accept().await.unwrap().0,
             colour: Player::Red,
         };
-        let player2 = PlayersConnection {
+        let mut player2 = PlayersConnection {
             socket: listener.accept().await.unwrap().0,
             colour: Player::Blue,
         };
+
+        // Randomize starting player
+        if rand::random() {
+            mem::swap(&mut player1, &mut player2);
+        }
 
         tokio::spawn(connections::game_handler(player1, player2));
     }
